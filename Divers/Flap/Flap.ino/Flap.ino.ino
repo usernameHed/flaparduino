25eb1e19cd2a7b746817287d54931c4afe463154
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-02-22 14:25:51 +0100

    Add aduino

---
+++ Divers/Flap/Flap.ino/Flap.ino.ino
@@ -0,0 +1,40 @@
+/*
+ * 
+ */
+ 
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup()
+{
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH)
+  {
+    //Serial.println("Up");
+    Serial.write(1);
+    Serial.flush();
+    delay(20);
+  }
+  else
+  {
+    //Serial.println("Down");
+    Serial.write(2);
+    Serial.flush();
+    delay(20);
+
+  }
+}
